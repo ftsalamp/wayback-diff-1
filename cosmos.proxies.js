e30f0bff755c24da9267201c7834d81735a46351
Fotis Tsalampounis <ftsalamp@gmail.com>
2018-10-21 00:14:31 +0100

    Add fetch proxy

---
+++ cosmos.proxies.js
@@ -0,0 +1,6 @@
+import createFetchProxy from 'react-cosmos-fetch-proxy';
+
+export default [
+  // Not all proxies have options, and often relying on defaults is good enough
+  createFetchProxy()
+];
